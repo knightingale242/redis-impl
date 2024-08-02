224202d08651a9149197fc4b1338e0769b3a91b5
Keonte <knightingale@usf.edu>
2024-08-01 23:53:24 -0400

    implemented query creator and parser

--- server.cpp
+++ server.cpp
@@ -7,6 +7,7 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
+#include<cassert>
 
 
 static void msg(const char *msg) {
@@ -30,6 +31,79 @@ static void do_something(int connfd) {
 
     char wbuf[] = "world";
     write(connfd, wbuf, strlen(wbuf));
+}
+
+static int32_t read_full(int fd, char *buf, size_t n){
+    while (n > 0){
+        ssize_t rv = read(fd, buf, n);
+        if (rv <= 0){
+            return -1; //error or unexpected EOF
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+static int32_t write_all(int fd, char *buf, size_t n){
+    while (n > 0){
+        ssize_t rv = write(fd, buf, n);
+        if (rv <= 0){
+            return -1; //error
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv; //decrements n by rv bytes
+        buf += rv; //increments pointer by rv bytes
+    }
+    return 0;
+}
+
+const size_t k_max_msg = 4096;
+
+//parser to handle incoming requests
+static int32_t one_request(int connfd){
+    //4 byte header
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+    int32_t err = read_full(connfd, rbuf, 4);
+    if (err){
+        if (errno == 0){
+            msg("EOF");
+        }
+        else{
+            msg("read() error");
+            return err;
+        }
+    }
+
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4);
+    if (len > k_max_msg){
+        msg("too long");
+        return -1;
+    }
+    
+    //request body
+    err = read_full(connfd, &rbuf[4], len);
+    if (err){
+        msg("read() error");
+        return err;
+    }
+
+    //processing
+    rbuf[4 + len] = '\0';
+    printf("client says %s\n", &rbuf[4]);
+
+    //reply using same protocol
+    const char reply[] = "world";
+    char wbuf[4 + sizeof(reply)];
+    len = (uint32_t)strlen(reply);
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], reply, len);
+    return write_all(connfd, wbuf, 4 + len);
+
+
 }
 
 int main() {
@@ -67,7 +141,13 @@ int main() {
             continue;   // error
         }
 
-        do_something(connfd);
+        while (true){
+            int32_t err = one_request(connfd);
+            if (err){
+                break;
+            }
+        }
+
         close(connfd);
     }
 
